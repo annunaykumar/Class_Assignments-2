9653cd094222e65f165c08097d4c7f02e4e2a776
annun <annun@DESKTOP-TUF8GF9>
2022-08-24 10:04:13 +0530

    Add project files.

---
+++ Class_Assignments-2/Q1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Assignments_2
+{
+    internal class Q1
+    {
+        // public static void Main()
+        //{
+        // class bank
+        // {
+        private double balance = 100000;
+
+
+        public double bal
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+    }
+    class fuctions
+    {
+        Q1 i = new Q1();
+        string name;
+        int account;
+        double withdraw, dep, total;
+
+        public void fun1()
+        {
+            Console.WriteLine("Enter Name of the depositor :");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter Account Number  :");
+            account = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Deposit Amount :");
+            dep = Convert.ToDouble(Console.ReadLine());
+            total = i.bal + dep;
+
+            Console.WriteLine("------------------------------\n");
+            Console.WriteLine("Name of the depositor : " + name);
+            Console.WriteLine("Account Number: " + account);
+            Console.WriteLine("Total Balance amount in the account  : " + total);
+
+        }
+        public void fun2()
+        {
+            Console.WriteLine("Enter Account Name :");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter Account Number  :");
+            account = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Withdraw Amount :");
+            withdraw = Convert.ToDouble(Console.ReadLine());
+            if (withdraw <= i.bal)
+            {
+                total = i.bal - withdraw;
+                Console.WriteLine("------------------------------\n");
+                Console.WriteLine("Account Name : " + name);
+                Console.WriteLine("Account Number: " + account);
+                Console.WriteLine("After Withdraw Amount balnace is : " + total);
+            }
+            else
+                Console.WriteLine("\n\nWithdraw Ammount does not Exist your Account.");
+        }
+
+    }
+}
---
+++ Class_Assignments-2/Q4.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Assignments_2
+{
+    internal class Q4
+    {
+        public float length, breadth, radius, side, hieght, breadthfortriangle;
+        public static void Main()
+        {
+            Q4 a = new Q4();
+            a.Rectangle();
+            a.Circle();
+            a.Square();
+            a.Triangle();
+            Console.ReadKey();
+        }
+        public void Rectangle()
+        {
+            Console.WriteLine("Enter the Length for Rectangle");
+            length = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the breadth for Rectangle");
+            breadth = float.Parse(Console.ReadLine());
+            System.Threading.Thread.Sleep(2000);
+            Console.WriteLine("Area of rectangle is :{0}", length * breadth);
+        }
+        public void Circle()
+        {
+            Console.WriteLine("Enter the Radius of the Circle");
+            radius = float.Parse(Console.ReadLine());
+            System.Threading.Thread.Sleep(2000);
+            Console.WriteLine("Area of Circle is:{0}", 3.14 * radius * radius);
+        }
+        public void Square()
+        {
+            Console.WriteLine("Enter the side of a square");
+            side = float.Parse(Console.ReadLine());
+            System.Threading.Thread.Sleep(2000);
+            Console.WriteLine("Area of Square is:{0}", side * side);
+        }
+        public void Triangle()
+        {
+            Console.WriteLine("Enter the Breadth for Triangle ");
+            breadthfortriangle = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Hieght for Triangle ");
+            hieght = float.Parse(Console.ReadLine());
+            System.Threading.Thread.Sleep(2000);
+            Console.WriteLine("Area of Triangle is:{0}", (breadthfortriangle * hieght) / 2);
+        }
+    }
+}
